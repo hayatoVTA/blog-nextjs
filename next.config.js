9d33c4eaca4cc556e70f37cfd77006f3e70490d3
tommy <29136901+tommylife88@users.noreply.github.com>
2021-03-08 15:33:00 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-/* eslint-env node */
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const path = require('path');
-
-module.exports = {
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      src: path.join(__dirname, 'src/'),
-    };
-    return config;
-  },
+module.exports = (phase, { defaultConfig }) => {
+  return defaultConfig;
 };
